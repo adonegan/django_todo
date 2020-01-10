99e7b8b4d287b755eb19b139b833014b6918accc
Aileen Donegan <aileendonegan@Aileens-MacBook-Air.local>
2020-01-10 00:27:02 +0000

    Updating IP and PORT

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'),
+                 os.environ.get('IP'),
+                 os.environ.get('PORT')]
 
 
 # Application definition
